3fa8379a6f04ecb908717849e894c0b7e0ffdf5d
gaston <gaston2600@gmail.com>
2022-10-13 17:22:12 +0100

    fix : update followTeachers

--- src/components/modules/Teachers/TeachersScreen.tsx
+++ src/components/modules/Teachers/TeachersScreen.tsx
@@ -1,20 +1,38 @@
-import {FlatList, RefreshControl, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  Pressable,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getTeachersList} from '../../../store/actions/teachersActions';
 import TeacherCmp from './components/TeacherCmp';
 import colors from '../../../styles/colors';
+import Icons from '../../../styles/icons';
+import I18n from 'react-native-i18n';
+import fonts from '../../../theme/fonts';
+import {Divider} from '@rneui/themed';
 
 const TeachersScreen = (props: any) => {
   const {navigation} = props;
   const dispatch = useDispatch();
-  const state = useSelector((state: any) => state);
   const {loading} = useSelector((state: any) => state?.Teachers);
   const [list, setList] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
   function getPage() {
     dispatch(
       getTeachersList(
-        {},
+        {
+          filters: {
+            searchInput,
+          },
+        },
         (res: any) => {
           // console.log({ res });
           setList(res?.teachers);
@@ -27,16 +45,57 @@ const TeachersScreen = (props: any) => {
   }
   useEffect(() => {
     getPage();
-  }, []);
+  }, [searchInput]);
 
   return (
     <View style={styles.containerStyle}>
+      <View style={styles.headerContainerStyle}>
+        <Pressable
+          onPress={() => {
+            navigation?.goBack();
+          }}>
+          <Icons.AntDesign
+            name="left"
+            size={20}
+            //   color={colors.darkGray}
+          />
+        </Pressable>
+        {!showSearchInput ? (
+          <Text style={styles.titleTextStyle}>{I18n.t('teachers')}</Text>
+        ) : (
+          <TextInput
+            // ref={refSearchInput}
+            value={searchInput}
+            onChangeText={setSearchInput}
+            style={styles.textInputStyle}
+            placeholder={I18n.t('search')}
+            focusable
+          />
+        )}
+        <Pressable
+          onPress={() => {
+            setShowSearchInput(!showSearchInput);
+            if (!showSearchInput) {
+              //   refSearchInput?.current?.focus();
+            } else {
+              setSearchInput('');
+            }
+          }}
+          style={styles.closeContainerStyle}>
+          <Icons.AntDesign
+            name={showSearchInput ? 'close' : 'search1'}
+            size={20}
+            color={colors.black}
+          />
+        </Pressable>
+      </View>
+      <Divider orientation="horizontal" />
       <FlatList
         data={list}
         renderItem={({item}) => (
-          <TeacherCmp data={item} navigation={navigation} />
+          <TeacherCmp data={item} navigation={navigation} refresh={getPage} />
         )}
-        keyExtractor={item => item?._id}
+        keyExtractor={(item: any) => item?._id}
         refreshControl={
           <RefreshControl
             style={{width: 0, height: 0}}
@@ -58,5 +117,28 @@ const styles = StyleSheet.create({
   containerStyle: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  textInputStyle: {
+    fontFamily: fonts.type.NunitoMedium,
+    fontSize: fonts.size.font12,
+    color: colors.gray,
+    paddingHorizontal: 5,
+    flex: 8,
+  },
+  headerContainerStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    height: 60,
+  },
+  titleTextStyle: {
+    fontFamily: fonts.type.NunitoSemiBold,
+    fontSize: fonts.size.font14,
+  },
+  closeContainerStyle: {
+    // position: 'absolute',
+    // left: 15,
+    // right: 15,
   },
 });
--- src/components/modules/Teachers/components/TeacherCmp.tsx
+++ src/components/modules/Teachers/components/TeacherCmp.tsx
@@ -1,133 +1,170 @@
-import { ActivityIndicator, Pressable, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+/* eslint-disable react-native/no-inline-styles */
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import fonts from '../../../../theme/fonts';
 import colors from '../../../../styles/colors';
 import AvatarCmp from '../../../common/AvatarCmp';
-import { extractImage } from '../../../../helpers/extractImage';
-import I18n from "react-native-i18n"
+import {extractImage} from '../../../../helpers/extractImage';
+import I18n from 'react-native-i18n';
 import globalStyles from '../../../../styles/globalStyles';
-import SelectInstitutionModal from '../../../modals/institutions/SelectInstitutionModal';
-import { useDispatch } from 'react-redux';
-import { getTeachersList, subscribeToTeacher } from '../../../../store/actions/teachersActions';
+import {useDispatch, useSelector} from 'react-redux';
+import {subscribeToTeacher} from '../../../../store/actions/teachersActions';
 
 const TeacherCmp = (props: any) => {
-    const { data, navigation, hideFollow } = props;
-    const dispatch = useDispatch()
-    // console.log({ data });
-    const [visibleSelectInst, setVisibleSelectInst] = useState(false)
-    const [loadingFollow, setLoadingFollow] = useState(false)
+  const {data, navigation, hideFollow, refresh = () => null} = props;
+  const dispatch = useDispatch();
+  const [loadingFollow, setLoadingFollow] = useState(false);
+  const [isSub, setIsSub] = useState(false);
+  const {selectedSpace} = useSelector((state: any) => state?.User);
 
-    function confirmSelecInstModal(params: any) {
-        setLoadingFollow(true)
-        dispatch(
-            subscribeToTeacher(
-                {
-                    teacher: data?._id,
-                    data: {
-                        partner: params?._id
-                    }
-                },
-                (res: any) => {
-                    dispatch(getTeachersList(
-                        {},
-                        () => null,
-                        () => null
-                    ))
-
-                    setLoadingFollow(false)
-                    setVisibleSelectInst(false)
-                },
-                (err: any) => {
-                    setLoadingFollow(false)
-                }
-            )
-        )
+  function isSubscribed() {
+    setIsSub(
+      data?.subscribers?.some((v: any) => v?.partner === selectedSpace?._id),
+    );
+  }
+  function subscribe() {
+    setLoadingFollow(true);
+    let temp;
+    if (selectedSpace?.type === 'Partner') {
+      temp = {
+        partner: selectedSpace?._id,
+      };
+    } else {
+      temp = {
+        institution: selectedSpace?._id,
+      };
     }
+    dispatch(
+      subscribeToTeacher(
+        {
+          teacher: data?._id,
+          data: temp,
+        },
+        () => {
+          refresh();
 
-    return (
-        <Pressable
-            onPress={() => {
-                if (!hideFollow) {
-                    navigation?.navigate("TeacherDetailsScreen", { teacher: data?._id })
-                }
-            }}
-            style={[styles.containerStyle, globalStyles.shadow]}>
-            <View style={{
-                flex: 1
-            }}>
-                <AvatarCmp
-                    name={String(data?.first_name)?.slice(0, 2)}
-                    uri={extractImage(data?.avatar?.path)}
-                    size={45}
-                />
-            </View>
+          setLoadingFollow(false);
+        },
+        (err: any) => {
+          setLoadingFollow(false);
+          console.log({err});
+        },
+      ),
+    );
+  }
 
-            <View style={{
-                flex: 5,
-                paddingLeft: 10
-            }} >
-                <Text style={styles.titleTextStyle}>{`${data?.first_name} ${data?.last_name}`}</Text>
-                <Text style={styles.descTextStyle}>{I18n.t(data?.partnertype)}</Text>
-            </View>
-            <View style={{
-                flex: 1.5,
-                // alignItems: "center",
-                justifyContent: "center"
-            }}>
-                {hideFollow ? null : <Pressable
-                    onPress={() => {
-                        setVisibleSelectInst(true)
-                    }}
-                    style={styles.followContainerStyle}>
-                    {loadingFollow ?
-                        <ActivityIndicator color={colors.white} size="small" /> :
-                        <Text style={styles.followTextStyle}>
-                            {I18n.t("follow")}
-                        </Text>}
-                </Pressable>}
-            </View>
-            <SelectInstitutionModal
-                visible={visibleSelectInst}
-                setVisible={setVisibleSelectInst}
-                confirm={confirmSelecInstModal}
-                selectedList={data?.subscribers?.map((v: any) => v?.institution?._id ? ({ _id: v?.institution?._id, type: "Institution" }) : ({ _id: v?.partner, type: "Partner" }))}
-            />
-        </Pressable>
-    )
-}
+  useEffect(() => {
+    isSubscribed();
+  }, [selectedSpace, data]);
 
-export default TeacherCmp
+  return (
+    <Pressable
+      onPress={() => {
+        if (!hideFollow) {
+          navigation?.navigate('TeacherDetailsScreen', {teacher: data?._id});
+        }
+      }}
+      style={[styles.containerStyle, globalStyles.shadow]}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <AvatarCmp
+          name={String(data?.first_name)?.slice(0, 2)}
+          uri={extractImage(data?.avatar?.path)}
+          size={45}
+        />
+      </View>
+
+      <View
+        style={{
+          flex: 5,
+          paddingLeft: 10,
+        }}>
+        <Text
+          style={
+            styles.titleTextStyle
+          }>{`${data?.first_name} ${data?.last_name}`}</Text>
+        <Text style={styles.descTextStyle}>{I18n.t(data?.partnertype)}</Text>
+      </View>
+      <View
+        style={{
+          flex: 1.5,
+          // alignItems: "center",
+          justifyContent: 'center',
+        }}>
+        {hideFollow ? null : (
+          <Pressable
+            onPress={subscribe}
+            style={[
+              styles.followContainerStyle,
+              !isSub && {
+                backgroundColor: colors.white,
+                borderColor: colors.primary,
+              },
+            ]}>
+            {loadingFollow ? (
+              <ActivityIndicator
+                color={isSub ? colors.white : colors.primary}
+                size="small"
+              />
+            ) : (
+              <Text
+                style={[
+                  styles.followTextStyle,
+                  !isSub && {color: colors.primary},
+                ]}>
+                {isSub ? 'Abonnée' : I18n.t('follow')}
+              </Text>
+            )}
+          </Pressable>
+        )}
+      </View>
+    </Pressable>
+  );
+};
+
+export default TeacherCmp;
 
 const styles = StyleSheet.create({
-    containerStyle: {
-        margin: 10,
-        // borderWidth: 1,
-        borderRadius: 5,
-        padding: 10,
-        flexDirection: "row",
-        // width: "100%"
-    },
-    titleTextStyle: {
-        fontFamily: fonts.type.NunitoMedium,
-        fontSize: fonts.size.font12,
-        color: colors.darkBlue
-    },
-    descTextStyle: {
-        fontFamily: fonts.type.NunitoRegular,
-        fontSize: fonts.size.font10,
-        color: colors.gray
-    },
-    followContainerStyle: {
-        padding: 5,
-        // paddingHorizontal: 10,
-        borderRadius: 20,
-        backgroundColor: colors.sereneBlue,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    followTextStyle: {
-        fontFamily: fonts.type.NunitoRegular,
-        fontSize: fonts.size.font10,
-        color: colors.white
-    },
-})
+  containerStyle: {
+    margin: 10,
+    // borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    flexDirection: 'row',
+    // width: "100%"
+  },
+  titleTextStyle: {
+    fontFamily: fonts.type.NunitoMedium,
+    fontSize: fonts.size.font12,
+    color: colors.darkBlue,
+  },
+  descTextStyle: {
+    fontFamily: fonts.type.NunitoRegular,
+    fontSize: fonts.size.font10,
+    color: colors.gray,
+  },
+  followContainerStyle: {
+    padding: 5,
+    // paddingHorizontal: 10,
+    borderRadius: 20,
+    backgroundColor: colors.sereneBlue,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: colors.sereneBlue,
+    height: 30,
+  },
+  followTextStyle: {
+    fontFamily: fonts.type.NunitoRegular,
+    fontSize: fonts.size.font10,
+    color: colors.white,
+  },
+});
